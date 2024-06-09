22663bd855c75730a93eb126c700db238aa15afb
Miami <alex.miami.petrov@gmail.com>
2024-06-09 17:08:55 +0300

    m

--- js/task-2.js
+++ js/task-2.js
@@ -22,5 +22,20 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+
+const galleryList = document.querySelector(".gallery");
+
+images.forEach((image) => {
+  const li = document.createElement("li");
+  const img = document.createElement("img");
+
+  img.src = image.url;
+  img.alt = image.alt;
+
+  li.appendChild(img);
+  galleryList.appendChild(li);
+});
+
+// galleryList.classList.add("gallery-style");
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const loginForm = document.querySelector(".login-form");
+const emailInput = loginForm.elements.email;
+const passwordInput = loginForm.elements.password;
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const emailValue = emailInput.value.trim();
+  const passwordValue = passwordInput.value.trim();
+
+  if (!emailValue || !passwordValue) {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+
+  loginForm.reset();
+});
