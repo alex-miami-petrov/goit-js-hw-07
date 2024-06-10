c7a4d650fa1e4fd81afccf0d1a7597e9a892ffae
Miami <alex.miami.petrov@gmail.com>
2024-06-10 15:55:53 +0300

    m

--- js/task-4.js
+++ js/task-4.js
@@ -1,24 +1,51 @@
+// const loginForm = document.querySelector(".login-form");
+// const emailInput = loginForm.elements.email;
+// const passwordInput = loginForm.elements.password;
+// const button = document.querySelector('button[type="submit"]');
+// button.innerHTML = "Log in";
+
+// const emailInput = document.getElementById("emailInput");
+
+// emailInput.addEventListener("mouseover", function () {
+//   this.style.border = "1px solid #000";
+// });
+
+// loginForm.addEventListener("submit", (event) => {
+//   event.preventDefault();
+
+//   const emailValue = emailInput.value.trim();
+//   const passwordValue = passwordInput.value.trim();
+
+//   if (!emailValue || !passwordValue) {
+//     alert("All form fields must be filled in");
+//     return;
+//   }
+
+//   const formData = {
+//     email: emailValue,
+//     password: passwordValue,
+//   };
+
+//   console.log(formData);
+
+//   loginForm.reset();
+// });
+
 const loginForm = document.querySelector(".login-form");
-const emailInput = loginForm.elements.email;
-const passwordInput = loginForm.elements.password;
 const button = document.querySelector('button[type="submit"]');
 button.innerHTML = "Log in";
 
-const emailInput = document.getElementById("emailInput");
-
-// Додаємо обробник подій для події mouseover
 emailInput.addEventListener("mouseover", function () {
-  // Змінюємо колір рамки на #000 (чорний)
   this.style.border = "1px solid #000";
 });
 
-loginForm.addEventListener("submit", (event) => {
+loginForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  const emailValue = emailInput.value.trim();
-  const passwordValue = passwordInput.value.trim();
+  const emailValue = this.elements.email.value.trim();
+  const passwordValue = this.elements.password.value.trim();
 
-  if (!emailValue || !passwordValue) {
+  if (emailValue === "" || passwordValue === "") {
     alert("All form fields must be filled in");
     return;
   }
@@ -30,5 +57,5 @@ loginForm.addEventListener("submit", (event) => {
 
   console.log(formData);
 
-  loginForm.reset();
+  this.reset();
 });
